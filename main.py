f5bdbe7dcf6a0ad9e1257059a495fe75922b57db
P <arnaudo.paolo20@gmail.com>
2024-12-09 22:48:35 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,103 @@
+import json
+import requests
+import bs4
+import sys
+import pandas as pd
+
+def usage():
+    print("--------USAGE--------")
+    print("Please provide one of the following flag upon running the script:")
+    print("'-p' -> heavy scraping")
+    print("'-l' -> light scraping")
+    print("Ex. 'python3 [python_script].py -p|-l'")
+
+
+#Extract url from regex ["first_page"-"last_page"]
+def extract_url(url):
+    out = []
+    result = []
+    url = list(url)
+    first_page = int(url[(url.index("[")) + 1])
+    last_page_list = url[(url.index("[") +3): url.index("]")]
+    last_page_raw = map(str, last_page_list)
+    last_page = "".join(last_page_raw)
+    last_page = int(last_page)
+    for i in range(first_page, last_page + 1):
+        del url[(url.index("[") +1) : url.index("]")]
+        url.insert(url.index("[") + 1, i)
+        url_string = map(str, url)
+        out.append("".join(url_string))
+    for item in out:
+        x = item.replace("[", "")
+        y = x.replace("]", "")
+        result.append(y)
+    return result
+
+#Initialize Dataframe to collect scraped data
+df = pd.DataFrame()
+i = int(0)
+
+#Importing json file where selectors are declared
+with open("input.json", "r") as selectors_file:
+    scrape_env = json.load(selectors_file)
+
+#Inner useful variables to isolate key/value pairs made of css selectors only.
+keys = list(scrape_env.keys())
+excl_keys = ["url", "box"]
+
+#Creating a dict with selectors only (therefore excluding "url" and "box")
+selectors_dict = {key: scrape_env[key] for key in scrape_env if key not in excl_keys}
+
+#Checking scraping type through flags ('-l' -> light scraping | '-p' -> heavy scraping)
+try:
+    #Light scraping
+    if sys.argv[1] == "-l":
+        inputL = [url for url in scrape_env["url"]] #Creating inner List to merge the lists returned by extract_url()
+        urls_list = []
+        for innerUrl in inputL:
+            innerL = extract_url(innerUrl)
+            urls_list += innerL #Merging the Lists returned by extract_url() together
+        for url in urls_list:
+            print("Scraping url: ", url)
+            scraped_page = requests.get(url) #Request to the url
+            scraped_page_soup  = bs4.BeautifulSoup(scraped_page.text, "html.parser") #Creating Soup
+            print("--------------------")
+            for box in scraped_page_soup.select(scrape_env["box"]): #For loop to gather data from every box
+                for selector in selectors_dict:
+                    if selectors_dict[selector][1] == "getText": #Establish where InnerText or tag attribute is to be scraped
+                        print(selector, box.select_one(selectors_dict[selector][0]).getText())
+                        df.loc[i, keys[0]] = url
+                        df.loc[i, selector] = box.select_one(selectors_dict[selector][0]).getText() #Insert data into dataframe
+                    else:
+                        print(selector, box.select_one(selectors_dict[selector][0]).get(selectors_dict[selector][1]))
+                        df.loc[i, keys[0]] = url
+                        df.loc[i, selector] = box.select_one(selectors_dict[selector][0]).get(selectors_dict[selector][1])
+                        print("--------------------")
+                i += 1
+
+    #Heavy scraping
+    elif sys.argv[1] == "-p":
+        urls_list = scrape_env["url"]
+        for url in urls_list:
+            print("Scraping url: ", url)
+            scraped_page = requests.get(url)
+            scraped_page_soup  = bs4.BeautifulSoup(scraped_page.text, "html.parser")
+            print("--------------------")
+            for selector in selectors_dict:
+                if selectors_dict[selector][1] == "getText":
+                    print(selector, scraped_page_soup.select_one(selectors_dict[selector][0]).getText())
+                    df.loc[i, keys[0]] = url
+                    df.loc[i, selector] = scraped_page_soup.select_one(selectors_dict[selector][0]).getText()
+                else:
+                    print(selector, scraped_page_soup.select_one(selectors_dict[selector][0]).get(selectors_dict[selector][1]))
+                    df.loc[i, keys[0]] = url
+                    df.loc[i, selector] = scraped_page_soup.select_one(selectors_dict[selector][0]).get(selectors_dict[selector][1])
+            print("--------------------")
+            i += 1
+
+except:
+    print("!! NO FLAG PROVIDED !!")
+    usage()
+
+
+df.to_csv("scraped_data.csv") #Producing output sheet
